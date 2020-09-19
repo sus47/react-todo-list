3ea0a28d002028ec896cc3df2da8fcffee0fd63e
Sushant Thapa <sushant.chhetri@>
2020-09-19 21:59:05 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoList from './component/TodoList';
+import TodoInput from './component/TodoInput';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <TodoList />
+          <TodoInput />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
